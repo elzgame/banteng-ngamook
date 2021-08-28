93277f6a6d8b0ff2aed095a534454b6a6ff17cd4
elzgame <elzgamedev@gmail.com>
2021-08-28 20:11:44 +0700

    I dont know

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
@@ -16,6 +17,9 @@ public class GameManager : MonoBehaviour
     public GameObject gameOverPanel;
     public AudioClip soundMoo;
     public AudioClip soundSpawn;
+    private static int distance;
+    public TextMeshProUGUI distanceText;
+    public GameObject halanganPrefab;
 
     void Start()
     {
@@ -26,6 +30,18 @@ public class GameManager : MonoBehaviour
         StartCoroutine(SpawningKoruptor());
         StartCoroutine(SpawningOrangBaik());
         StartCoroutine(DelayStartSound());
+        StartCoroutine(CountScore());
+        StartCoroutine(Halangan());
+
+    }
+
+    IEnumerator CountScore()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1.0f);
+            distance++;
+        }
     }
 
     IEnumerator DelayStartSound()
@@ -38,6 +54,8 @@ public class GameManager : MonoBehaviour
 
     void Update()
     {
+        distanceText.text = "JARAK : " + distance.ToString() + "M";
+
         if (Player.health <= 0)
         {
             StartCoroutine(GameOver());
@@ -68,16 +86,26 @@ public class GameManager : MonoBehaviour
     {
         while (true)
         {
-            yield return new WaitForSeconds(Random.Range(2, 4));
+            yield return new WaitForSeconds(Random.Range(5, 10));
             Instantiate(bomb, spawner.transform.position, Quaternion.identity);
         }
     }
+
+    IEnumerator Halangan()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(Random.Range(5, 10));
+            var create = Instantiate(halanganPrefab, new Vector2(-10, spawner.transform.position.y), Quaternion.identity); 
+        }
+    }
+
 
     IEnumerator SpawningLives()
     {
         while (true)
         {
-            yield return new WaitForSeconds(Random.Range(4, 8));
+            yield return new WaitForSeconds(Random.Range(10, 20));
             Instantiate(lives, spawner.transform.position, Quaternion.identity);
         }
     }
@@ -86,7 +114,7 @@ public class GameManager : MonoBehaviour
     {
         while (true)
         {
-            yield return new WaitForSeconds(Random.Range(3, 10));
+            yield return new WaitForSeconds(Random.Range(10, 15));
             Instantiate(koruptor, spawner.transform.position, Quaternion.identity);
         }
     }
@@ -95,7 +123,7 @@ public class GameManager : MonoBehaviour
     {
         while (true)
         {
-            yield return new WaitForSeconds(Random.Range(5, 9));
+            yield return new WaitForSeconds(Random.Range(4, 13));
             Instantiate(orangBaik, spawner.transform.position, Quaternion.identity);
         }
     }
